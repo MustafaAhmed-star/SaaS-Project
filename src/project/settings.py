ec1568a7dc2ee0549ac7b75646fc32a4e66e593a
Mustafa  Abdelmgeed <mostafa012155@gmail.com>
2024-10-17 23:18:57 +0300

    prepare allowed_Hosts to productions

--- src/project/settings.py
+++ src/project/settings.py
@@ -25,8 +25,12 @@ SECRET_KEY = 'django-insecure-6)%i*+n!7pgwk3f8@8y&2+r#bz!naj)jr=uwghw+#kc!y@v@@(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.railway.app',
 
+]
+if DEBUG:
+    ALLOWED_HOSTS+=['localhost', '127.0.0.1']
 
 # Application definition
 
