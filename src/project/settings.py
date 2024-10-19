b792e5c8a679da71df7e5c9dbde7a4f28560a7ea
Mustafa  Abdelmgeed <mostafa012155@gmail.com>
2024-10-19 23:20:28 +0300

    perpare db neon postgres for production

--- src/project/settings.py
+++ src/project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-6)%i*+n!7pgwk3f8@8y&2+r#bz!naj)jr=uwghw+#kc!y@v@@('
 
 SECRET_KEY = config('DJANGO_SECRET_KEY')
-print(SECRET_KEY)
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG', cast=bool, default=False)
 
@@ -82,14 +82,25 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=300)
+DATABASE_URL = config("DATABASE_URL", default=None)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=CONN_MAX_AGE,
+            conn_health_checks=True,
+        )
     }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
--- src/project/views.py
+++ src/project/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from visits.models import PageVisit
 
 
 def home(request,*args,**kwargs):
-    context  = {}
+    qs = PageVisit.objects.all()
+    PageVisit.objects.create(path=request.path)
+    context  = {'page_visits': qs,
+                'page_visit_count': qs.count(),
+                'page_total_percentage': qs.count() / 100,
+                    }
     return render(request,'home.html',context)
